71a862db233f216b2376896c5bcbfc001e0af01c
Fuad0010 <fuadft@code.edu.az>
2022-04-07 17:02:39 +0400

    Add project files.

---
+++ ThreadAsyncDirectoryFile/Models/Status.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadAsyncDirectoryFile.Models
+{
+    internal class Status
+    {
+        private int _id;
+        public string Title;
+        public string Content;
+        public int Id 
+        { 
+            get 
+            { 
+                return _id; 
+            }
+            set 
+            {
+                _id = value; 
+            }
+        }
+
+
+    }
+}
